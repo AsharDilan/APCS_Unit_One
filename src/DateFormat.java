45782926f96fe9318ab2cfc02383332852757cb7
AsharDilan <ashardi@ssfs.org>
2021-09-13 11:56:55 -0400

    Date Format

--- src/DateFormat.java
+++ src/DateFormat.java
@@ -3,6 +3,19 @@ import java.util.Scanner;
 public class DateFormat {
 
     public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What is the current month?");
+    String month = scan.nextLine();
+    System.out.println("What is the current day of the week?");
+    String day = scan.nextLine();
+    System.out.println("What is the current year?");
+    int year = scan.nextInt();
+    System.out.println("What is the day of the month");
+    int dayOfMonth = scan.nextInt();
 
+    System.out.println("American Format:");
+    System.out.println(day + ", " + month + " " + dayOfMonth + ", " + year + "");
+    System.out.println("European Format:");
+    System.out.println(day + ", " + dayOfMonth + " " + month + ", " + year + "");
     }
 }
